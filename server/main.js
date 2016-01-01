97179938dedb675f3517741f616d487fed1e820f
stephenkaraga <stephenkaraga@gmail.com>
2015-12-31 21:03:04 -0500

    get jsx output to display in console

--- server/main.js
+++ server/main.js
@@ -1,9 +1,13 @@
 var express = require('express');
-
+var path = require('path');
 var app = new express();
+const appPath = path.resolve(__dirname, '../app');
+console.log(appPath)
 
 app.get('/', function(req, res){
-	res.render('./../app/views/index.ejs',{});
+	res.render('./../app/views/index.ejs',{})
 })
-.use(express.static(__dirname, + '/../app'))
-.listen(7777);
+
+.use('/app', express.static(appPath))
+
+.listen(7777)
