80fc5146d10a3c307307588fa27d10edbbc03431
stephenkaraga <stephenkaraga@gmail.com>
2016-01-02 05:29:17 -0500

    add isomorphic support

--- server/main.js
+++ server/main.js
@@ -3,11 +3,25 @@ var path = require('path');
 var parser = require('body-parser');
 var app = new express();
 const appPath = path.resolve(__dirname, '../.tmp');
+var React = require('react/addons');
 
+var GroceryItem = require('./models/GroceryItem.js');
+
+require("babel/register")
 require('./database.js');
 
 app.get('/', function(req, res){
-	res.render('./../app/views/index.ejs',{})
+	// res.render('./../app/views/index.ejs',{})
+	var application = React.createFactory(require('./../app/components/GroceryItemList.jsx'));
+
+	GroceryItem.find(function(err,docs){
+		var generated = React.renderToString(application({
+			items:docs
+		}));
+
+		res.render('./../app/views/index.ejs',{reactOutput:generated});
+	})
+
 })
 
 .use(express.static(appPath))
